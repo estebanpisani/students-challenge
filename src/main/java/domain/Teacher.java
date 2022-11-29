a0263b86735b7cc19172e92a0eba381a41a9fb2e
olivierfredericartur.sanchis.contractor.gdevtools.com <olivierfredericartur.sanchis.contractor@gdevtools.com>
2022-11-29 13:04:13 -0300

    Teacher Thread finished

--- src/main/java/domain/Teacher.java
+++ src/main/java/domain/Teacher.java
@@ -1,4 +1,12 @@
 package domain;
 
 public class Teacher extends Thread{
+    private WhiteBoard whiteBoard;
+
+    @Override
+    public void run() {
+        for (int i=0; i<50; i++){
+            whiteBoard.write(String.valueOf(i));
+        }
+    }
 }
